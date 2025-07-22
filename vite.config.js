abf6dc252c0ec8ee5c3a034383989fbee242305e
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2025-07-21 23:23:09 -0400

    maybe?

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://nate-seligson.github.io/irl-showcase/"
+  base: "/irl-showcase/"
 })
